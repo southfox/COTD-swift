c53e20eb7adc198f49caef402cf2976d87910b50
Javier Fuchs <Javier.X.Fuchs.-ND@disney.com>
2015-08-05 10:22:46 -0300

    Starting swift project

---
+++ COTD-swift/COTD-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  COTD-swift
+//
+//  Created by Javier Fuchs on 8/5/15.
+//  Copyright (c) 2015 Javier Fuchs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
